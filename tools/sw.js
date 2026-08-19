cef9d6c535ed1e15f7485d810ac60112ebf2dff2
山住虎太郎 <yamazumi.ipynb@gmail.com>
2026-08-19 13:13:12 +0900

    PWA化のための追加ファイル

---
+++ tools/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'marathon-rx-v2';
+const urlsToCache = [
+  './marathon-rx.html',
+  './manifest_marathon-rx.json',
+  './icon_marathon-rx.png',
+  './icon_marathon-rx_192.png'
+];
+
+// インストール時に指定したファイルをキャッシュ
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// リクエスト時にキャッシュがあればそれを返し、なければネットワークから取得
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
